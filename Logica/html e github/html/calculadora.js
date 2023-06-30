032f31d22dc1450d40753c5400131d7eb30767a6
Thiago <129191763+Thiago0g0@users.noreply.github.com>
2023-06-30 09:07:35 -0300

    Add files via upload

---
+++ Logica/html e github/html/calculadora.js
@@ -0,0 +1,7 @@
+function calcular (n1, n2){
+    n1 = paeseInt (document.getElementById("primeiro").value)
+    n1 = parceInt (document.getElementById("segundo").value)
+    let result = n1 + n2 
+    document.getElementById("resuldado").innerHTML = result
+
+}
